79c82696c0741556849d406b1035d7ebc09cfa6e
MM <manufacturevn@gmail.com>
2021-01-03 15:28:10 +0700

    Adding all files & folders under the project

---
+++ build/CMakeFiles/hello_apartment.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/hello_apartment.dir/script/hello_apartment.cpp.o"
+  "libhello_apartment.pdb"
+  "libhello_apartment.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/hello_apartment.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
